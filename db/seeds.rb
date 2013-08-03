730dff0a1b46ecb8be018a42d749af9e3e8a4009
Igor Prado <igorprado@gmail.com>
2013-08-03 00:51:08 -0300

    updated faker values

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ require 'ffaker'
 
 100.times do |n|
   Article.create!(
-    title: Faker::Lorem.sentence(2),
+    title: Faker::Lorem.words(4).join(' ').capitalize,
     summary: Faker::Lorem.paragraph,
     body: Faker::Lorem.paragraphs.join(" "),
     category: Category.all.sample)
